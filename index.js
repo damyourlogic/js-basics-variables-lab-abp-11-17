a36e0af5bb2e938688466f5ec2fc285121c2b90a
Amy Jackson <aj81623@gmail.com>
2017-11-06 00:22:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solution in this file!
 const companyName = 'Scuber';
 let mostProfitableNeighborhood = 'Chelsea';
+<<<<<<< HEAD
 let companyCeo = 'Susan Smith';
+=======
+let companyCeo = 'Susan Smith';
+>>>>>>> 91e4f51489f2212d2ae914cec1bb183e73c4600e
